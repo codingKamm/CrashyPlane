a77b4514dc9e6ec99014dc2cafbdef2f749f76dc
Cameron Warner <cameron.w.warner@gmail.com>
2023-09-24 16:53:58 -0400

    Initial Commit

---
+++ MotorRun macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MotorRun macOS
+//
+//  Created by Cameron Warner on 9/24/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
